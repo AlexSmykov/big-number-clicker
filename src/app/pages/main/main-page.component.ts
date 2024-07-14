87414a64cb430d343d394b2b808b9dd425e319d6
Алексей Смыков <asmykov@embedika.ru>
2024-07-14 22:46:28 +0700

    feat: добавлен компонент кнокпи для клика

--- src/app/pages/main/main-page.component.ts
+++ src/app/pages/main/main-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import ResourcesComponent from 'src/app/components/resources/resources.component';
+import ClickButtonComponent from 'src/app/components/click-button/click-button.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
   standalone: true,
-  imports: [ResourcesComponent],
+  imports: [ResourcesComponent, ClickButtonComponent],
 })
 export default class MainPageComponent {}
