52c54415fbc6b2cf0cbf5b9e99bfe23e721156ca
Алексей Смыков <asmykov@embedika.ru>
2024-07-14 22:44:20 +0700

    feat: добавлены данные в параметры и разблокирования

--- src/app/core/unlocks/unlocks.const.ts
+++ src/app/core/unlocks/unlocks.const.ts
@@ -3,4 +3,6 @@ import { EUnlocks } from 'src/app/core/unlocks/unlocks.enum';
 
 export const UNLOCKS_START_CONFIG: TUnlocks = {
   [EUnlocks.IS_PRESTIGE_UNLOCK]: false,
+  [EUnlocks.IS_RUBYS_UNLOCK]: false,
+  [EUnlocks.IS_LOG_MULTIPLIER_UNLOCK]: false,
 };
