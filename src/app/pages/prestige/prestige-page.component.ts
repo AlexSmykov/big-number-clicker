5635ad1d3a9b20282ec4238e0d1c76721549c496
Алексей Смыков <asmykov@embedika.ru>
2024-08-07 18:32:22 +0700

    feat: страница престижа

--- src/app/pages/prestige/prestige-page.component.ts
+++ src/app/pages/prestige/prestige-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { AsyncPipe } from '@angular/common';
+
+import { ResourcesService } from 'src/app/core/resources/resources.service';
+import { PrestigeService } from 'src/app/core/prestige/prestige.service';
+import { EResources } from 'src/app/core/resources/resources.enum';
 
 @Component({
   selector: 'app-prestige-page',
   templateUrl: './prestige-page.component.html',
   styleUrls: ['./prestige-page.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent, AsyncPipe],
 })
-export default class PrestigePageComponent {}
+export default class PrestigePageComponent {
+  private readonly resourceService = inject(ResourcesService);
+  private readonly prestigeService = inject(PrestigeService);
+
+  readonly prestigeButtonSelected = signal(false);
+
+  readonly currentPrestigeBorder$ = this.prestigeService.currentPrestigeBorder$;
+  readonly availableToPrestige$ =
+    this.prestigeService.availableToPrestigeByBorder$;
+  readonly availablePrestigePoints$ =
+    this.prestigeService.availablePrestigePointOnPrestige$;
+  readonly currentPrestigePoints$ = this.resourceService.getResource$(
+    EResources.PRESTIGE_POINT
+  );
+  readonly currentMoney$ = this.resourceService.getResource$(EResources.MONEY);
+
+  prestige(): void {
+    this.prestigeButtonSelected.set(false);
+    this.prestigeService.prestige();
+  }
+}
